5148214c82387b8402093f311cfa685424d6be5e
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-04-11 14:45:06 -0500

    profileRequestDto validation

--- src/main/java/org/example/userprofilems/dto/requestDto/ProfileRequestDto.java
+++ src/main/java/org/example/userprofilems/dto/requestDto/ProfileRequestDto.java
@@ -1,13 +1,24 @@
 package org.example.userprofilems.dto.requestDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDate;
 
 public record ProfileRequestDto(
+        @NotBlank(message = "First name can not be blank/empty/null")
         String firstName,
+        @NotBlank(message = "Last name can not be blank/empty/null")
         String lastName,
+        @NotNull(message = "Birth date can not be null")
         LocalDate birthDate,
+        @Email(message = "Email is not valid")
         String email,
+        @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
         String phoneNumber,
+        @NotBlank(message = "Bio can not be blank/empty/null")
         String bio
 ) {
 }
--- src/main/java/org/example/userprofilems/dto/requestDto/UserRequestDto.java
+++ src/main/java/org/example/userprofilems/dto/requestDto/UserRequestDto.java
@@ -1,6 +1,6 @@
 package org.example.userprofilems.dto.requestDto;
-
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
@@ -9,7 +9,9 @@ public record UserRequestDto(
         String userName,
         @NotBlank(message = "Password can not be blank/empty/null")
                 @Size(min = 8, message = "Password must be at least 8 characters long")
-        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[?*+!])[A-Za-z0-9?*+!]{4,8}$")        String password,
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[?*+!])[A-Za-z0-9?*+!]{4,8}$")
+        String password,
+        @NotNull(message = "Profile can not be null")
         ProfileRequestDto profileRequestDto
 ) {
 }
